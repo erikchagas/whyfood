b28ed2424e6d474d1d60930653c4b78f70e76740
Erik Chagas <erikchagas00@gmail.com>
2019-09-07 20:14:25 -0400

    Adicionar ao state propriedade 'products'

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Header, Container, Card, CardTitle, CardPrice } from './styles';
 
-function Main() {
-  return (
-    <>
-      <Header />
-      <Container>
-        <Card>
-          <CardTitle>ABC</CardTitle>
-          <CardPrice>ABC</CardPrice>
-        </Card>
-      </Container>
-    </>
-  );
-}
+export default class Main extends Component {
+  state = {
+    products: [],
+  };
+  render() {
+    const { products } = this.state;
 
-export default Main;
+    console.log(products);
+
+    return (
+      <>
+        <Header />
+        <Container>
+          <Card>
+            <CardTitle>ABC</CardTitle>
+            <CardPrice>ABC</CardPrice>
+          </Card>
+        </Container>
+      </>
+    );
+  }
+}
