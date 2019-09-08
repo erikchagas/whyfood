e7fff10c1cec26514248ed80461606cd16abd1ae
Erik Chagas <erikchagas00@gmail.com>
2019-09-08 18:47:34 -0400

    Adicionar carregamento do Modal, click no Card, no ModalClose

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 
 import {
- Header, Container, Card, CardTitle, CardPrice 
+  Header,
+  Container,
+  Card,
+  CardTitle,
+  CardPrice,
+  Modal,
+  ModalCard,
+  ModalHeader,
+  ModalClose,
+  ModalName,
+  ModalImg,
+  ModalPrice,
+  ModalContent,
+  ModalOption,
+  ModalOptionHeader,
+  ModalOptionTitle,
+  ModalOptionStatus,
+  ModalOptionList,
+  ModalOptionName,
+  ModalOptionPrice,
+  ModalOptionSelect,
+  ModalFooter,
+  ModalPicker,
+  ModalConfirm,
 } from './styles';
 
 import api from '../../services/api';
@@ -11,8 +34,9 @@ export default class Main extends Component {
     super(props);
     this.state = {
       products: [],
-      details: {},
+      detail: {},
     };
+    this.baseState = this.state;
   }
 
   async componentDidMount() {
@@ -25,30 +49,78 @@ export default class Main extends Component {
     });
   }
 
-  async handleClick(e) {
-    const response = await api.get(`/products/${e.target.id}`);
+  async handleClickCard(e) {
+    const { detail } = this.state;
 
+    const response = await api.get(`/products/${e.currentTarget.id}`);
+
+    if (response.data) {
+      this.setState({
+        detail: { ...detail, ...response.data },
+      });
+    }
+  }
+
+  handleClickModalClose = () =>{
     this.setState({
-      details: response.data,
+      detail: {},
     });
   }
 
   render() {
-    const { products, details } = this.state;
-
-    console.log(details);
-
+    const { products, detail } = this.state;
     return (
       <>
         <Header />
         <Container>
-          {products.map(product => (
-            <Card key={product.id} id={product.id} onClick={this.handleClick}>
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              id={product.id}
+              onClickCapture={(e) => this.handleClickCard(e)}
+            >
               <CardTitle>{product.name}</CardTitle>
               <CardPrice>{product.price}</CardPrice>
             </Card>
           ))}
         </Container>
+        {Object.getOwnPropertyNames(detail).length && (
+          <Modal key={detail.id} id={detail.id}>
+            <ModalCard>
+              <ModalHeader>
+                <ModalClose onClickCapture={this.handleClickModalClose} />
+                <ModalName>{detail.name}</ModalName>
+                <ModalPrice>{detail.price}</ModalPrice>
+                <ModalImg src={detail.image_url} />
+              </ModalHeader>
+              <ModalContent>
+                {detail.options.length
+                  && detail.options.map((option) => (
+                    <ModalOption key={option.id}>
+                      <ModalOptionHeader>
+                        <ModalOptionTitle>{option.title}</ModalOptionTitle>
+                        <ModalOptionStatus>
+                          {option.type !== 'single' ? '' : ''}
+                        </ModalOptionStatus>
+                      </ModalOptionHeader>
+                      {
+                        option.values.length && option.values.map((value) =>
+                          <ModalOptionList key={value.id}>
+                            <ModalOptionName>{value.name}</ModalOptionName>
+                            <ModalOptionPrice>{value.price}</ModalOptionPrice>
+                            <ModalOptionSelect />
+                          </ModalOptionList>
+                        )}
+                    </ModalOption>
+                  ))}
+              </ModalContent>
+              <ModalFooter>
+                <ModalPicker />
+                <ModalConfirm>Adicionar</ModalConfirm>
+              </ModalFooter>
+            </ModalCard>
+          </Modal>
+        )}
       </>
     );
   }
