8fb679c7ee25d906438166369de2d336ec0e9cef
Erik Chagas <erikchagas00@gmail.com>
2019-09-08 13:12:48 -0400

    Adicionar consulta por lista de produtos e carregamento da mesma

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 
-import { Header, Container, Card, CardTitle, CardPrice } from './styles';
+import {
+ Header, Container, Card, CardTitle, CardPrice 
+} from './styles';
+
+import api from '../../services/api';
 
 export default class Main extends Component {
-  state = {
-    products: [],
-  };
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      details: {},
+    };
+  }
+
+  async componentDidMount() {
     const { products } = this.state;
 
-    console.log(products);
+    const response = await api.get('/products');
+
+    this.setState({
+      products: [...products, ...response.data],
+    });
+  }
+
+  async handleClick(e) {
+    const response = await api.get(`/products/${e.target.id}`);
+
+    this.setState({
+      details: response.data,
+    });
+  }
+
+  render() {
+    const { products, details } = this.state;
+
+    console.log(details);
 
     return (
       <>
         <Header />
         <Container>
-          <Card>
-            <CardTitle>ABC</CardTitle>
-            <CardPrice>ABC</CardPrice>
-          </Card>
+          {products.map(product => (
+            <Card key={product.id} id={product.id} onClick={this.handleClick}>
+              <CardTitle>{product.name}</CardTitle>
+              <CardPrice>{product.price}</CardPrice>
+            </Card>
+          ))}
         </Container>
       </>
     );
